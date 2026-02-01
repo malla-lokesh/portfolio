43fb72c12f134f219c466dc79c76c7e3347735e3
lokesh mvs <lokeshmvs21@gmail.com>
2026-02-01 12:58:52 +0530

    chore: Update to latest version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center h-screen p-10">
-      <div>Cooking.</div>
-    </main>
-  );
+    return (
+        <main className="flex flex-col items-center justify-center h-screen p-10">
+            <div>Cooking.</div>
+        </main>
+    );
 }
